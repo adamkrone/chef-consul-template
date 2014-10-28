b6b715066693c32b11b272064f795648e2ecf04e
Adam Krone <krone.adam@gmail.com>
2014-10-28 12:30:19 -0500

    Tweak init stop command

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'krone.adam@gmail.com'
 license          'Apache v2.0'
 description      'Installs/Configures consul-template'
 long_description 'Installs/Configures consul-template'
-version          '0.1.0'
+version          '0.2.0'
 
 
 depends 'ark'
