f9634a13208f8357a1517ea5167eda6c9d1440d1
Adam Krone <krone.adam@gmail.com>
2014-11-26 21:10:15 -0600

    Adds kitchen test for config lwrp commands

--- test/integration/default/serverspec/template_lwrp_spec.rb
+++ test/integration/default/serverspec/template_lwrp_spec.rb
@@ -8,5 +8,10 @@ describe "consul_template_config LWRP" do
     it { should be_file }
     it { should contain 'source = "/tmp/test.config.ctmpl"' }
     it { should contain 'destination = "/tmp/test.config"' }
+    it { should contain 'command = "touch /tmp/consul-template-command-test"' }
+  end
+
+  describe file('/tmp/consul-template-command-test') do
+    it { should be_file }
   end
 end
