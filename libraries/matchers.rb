d9bf52e63aaef82f0aa4bc5c2aa3c90f609990c4
Adam Krone <krone.adam@gmail.com>
2014-12-15 16:51:40 -0600

    Rubocop fixes

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,4 +1,3 @@
 def create_consul_template_config(resource_name)
   ChefSpec::Matchers::ResourceMatcher.new(:consul_template_config, :create, resource_name)
 end
-
--- spec/fixtures/cookbooks/consul-template-spec/recipes/consul_template_config.rb
+++ spec/fixtures/cookbooks/consul-template-spec/recipes/consul_template_config.rb
@@ -7,9 +7,7 @@
 include_recipe 'consul-template'
 
 consul_template_config 'haproxy' do
-  templates [{
-     source: '/etc/haproxy/haproxy.cfg.ctmpl',
-     destination: '/etc/haproxy/haproxy.cfg',
-     command: 'service haproxy restart'
-   }]
+  templates [{ source: '/etc/haproxy/haproxy.cfg.ctmpl',
+               destination: '/etc/haproxy/haproxy.cfg',
+               command: 'service haproxy restart' }]
 end
