7b9e67dd69472be10f036e2cd68702fbc46d6006
Adam Renberg <tgwizard@gmail.com>
2015-01-17 20:36:39 +0100

    Refactor config provider to not reload service incorrectly
    The old reload notification didn't work when when the service provider
    upstart. The user of consul_template_config will now need to now to
    notify a reload of the consul-template service. This is a breaking
    change, but much cleaner and simpler to reason about. It also will not
    result in more than one reload (if :delayed is used).

--- providers/config.rb
+++ providers/config.rb
@@ -13,19 +13,11 @@ action :create do
     fail "Missing destination for #{i} entry at '#{new_resource.name}" if v[:destination].nil?
   end
 
-  # Placeholder for future consul-template service definitions...
-  service 'consul-template' do
-    ignore_failure true
-    action :nothing
-  end
-
-  # Only notifies if any template is provided
   template ::File.join(node['consul_template']['config_dir'], new_resource.name) do
     cookbook 'consul-template'
     source 'config-template.json.erb'
     mode node['consul_template']['template_mode']
     variables(:templates => templates)
-    notifies :reload, 'service[consul-template]', :delayed
     not_if { templates.empty? }
   end
 end
