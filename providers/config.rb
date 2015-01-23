7816b8bc405f08e75452a6ab6b0c0241826648e4
Ben Scott <gamepoet@gmail.com>
2015-01-23 13:10:39 -0800

    create config files with the service user when using runit

--- providers/config.rb
+++ providers/config.rb
@@ -7,6 +7,15 @@ end
 action :create do
   templates = new_resource.templates.map { |v| Mash.from_hash(v) }
 
+  case node['consul_template']['init_style']
+  when 'runit'
+    consul_template_user = node['consul_template']['service_user']
+    consul_template_group = node['consul_template']['service_group']
+  else
+    consul_template_user = 'root'
+    consul_template_group = 'root'
+  end
+
   # Create entries in configs-template dir but only if it's well formed
   templates.each_with_index do |v, i|
     fail "Missing source for #{i} entry at '#{new_resource.name}" if v[:source].nil?
@@ -16,6 +25,8 @@ action :create do
   template ::File.join(node['consul_template']['config_dir'], new_resource.name) do
     cookbook 'consul-template'
     source 'config-template.json.erb'
+    user consul_template_user
+    group consul_template_group
     mode node['consul_template']['template_mode']
     variables(:templates => templates)
     not_if { templates.empty? }
