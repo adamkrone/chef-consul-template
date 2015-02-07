4b0104a8fa40e598aed0bff66a10b4fd7272985d
Adam Krone <krone.adam@gmail.com>
2015-02-06 19:54:02 -0600

    Adds recipe/support info to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,16 @@ description      'Installs/Configures consul-template'
 long_description 'Installs/Configures consul-template'
 version          '0.6.5'
 
+recipe 'consul-template', 'Installs, configures, and starts the consul-template service.'
+recipe 'consul-template::install_binary', 'Installs consul-template from binary.'
+recipe 'consul-template::install_source', 'Installs consul-template from source.'
+recipe 'consul-template::service', 'Configures and starts the consul-template service.'
+
+supports 'ubuntu', '= 14.04'
+supports 'ubuntu', '= 12.04'
+supports 'debian', '= 7.7'
+supports 'debian', '= 6.0.10'
+supports 'centos', '= 6.5'
 
 depends 'ark'
 depends 'golang', '~> 1.4.0'
