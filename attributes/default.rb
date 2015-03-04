26b9e40b894f5e2518104401e65b5ef94f43be96
Franklin Wise <franklin@krave.io>
2015-03-04 11:35:29 -0800

    Moved hashes to bottom of file
    Since the hash list will grow endlessly, let's put it on the bottom of the file, that way the handful of attributes that are available to customize are all at the top and together.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,20 @@ default['consul_template']['base_url'] = 'https://github.com/hashicorp/consul-te
 default['consul_template']['version'] = '0.7.0'
 default['consul_template']['install_method'] = 'binary'
 default['consul_template']['install_dir'] = '/usr/local/bin'
+
+default['consul_template']['source_revision'] = 'master'
+
+# Service attributes
+default['consul_template']['log_level'] = 'info'
+default['consul_template']['config_dir'] = '/etc/consul-template.d'
+default['consul_template']['init_style'] = 'init'   # 'init', 'runit'
+default['consul_template']['service_user'] = 'consul-template'
+default['consul_template']['service_group'] = 'consul-template'
+default['consul_template']['template_mode'] = 0600
+
+# Config attributes
+default['consul_template']['config'] = Hash.new
+
 default['consul_template']['checksums'] = {
   'consul-template_0.5.1_darwin_386' => 'e410ac125fecd61e0abed9edbe626e40da526fffe474791af07d89fcc23da98b',
   'consul-template_0.5.1_darwin_amd64' => '32aeb5e50bf00fc46c3ae195a7d43ae539ce13115893b18bb9f2d63e373bed7e',
@@ -72,15 +86,3 @@ default['consul_template']['checksums'] = {
   'consul-template_0.7.0_windows_386' => '2799405bb39b23da69a82d84ffbc8ddb6c0b73f6de04e45d9030ec4ee9a92788',
   'consul-template_0.7.0_windows_amd64' => 'b527bc794ff8cacb43a8b31fb508b834d76272abcbf81756ea301e34c1f0a5d0',
 }
-default['consul_template']['source_revision'] = 'master'
-
-# Service attributes
-default['consul_template']['log_level'] = 'info'
-default['consul_template']['config_dir'] = '/etc/consul-template.d'
-default['consul_template']['init_style'] = 'init'   # 'init', 'runit'
-default['consul_template']['service_user'] = 'consul-template'
-default['consul_template']['service_group'] = 'consul-template'
-default['consul_template']['template_mode'] = 0600
-
-# Config attributes
-default['consul_template']['config'] = Hash.new
