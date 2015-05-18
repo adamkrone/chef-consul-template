2af931140bd2da9124a915e3144a5830da93d84f
Adam Krone <krone.adam@gmail.com>
2015-05-18 09:40:51 -0500

    Fixes consul_template_config test recipe/spec

--- spec/resources/consul_template_config_spec.rb
+++ spec/resources/consul_template_config_spec.rb
@@ -6,11 +6,19 @@ describe 'consul_template_config resource' do
 
   describe 'create' do
     it 'should create the consul_template_config' do
-      expect(chef_run).to create_consul_template_config('haproxy')
+      expect(chef_run).to create_consul_template_config('test')
     end
 
     it 'should create the config file' do
-      expect(chef_run).to create_template('/etc/consul-template.d/haproxy')
+      expect(chef_run).to create_template('/etc/consul-template.d/test')
+    end
+
+    it 'should create the ctmplfile' do
+      expect(chef_run).to create_template('/tmp/test.config.ctmpl')
+    end
+
+    it 'should add the test key/value' do
+      expect(chef_run).to run_execute('add test key/value')
     end
   end
 end
