05a32365da686a18abd224e5eb5b3f6190425fd5
Adam Krone <krone.adam@gmail.com>
2015-05-18 09:30:44 -0500

    Updates chefspec tests

--- spec/resources/consul_template_config_spec.rb
+++ spec/resources/consul_template_config_spec.rb
@@ -5,6 +5,10 @@ describe 'consul_template_config resource' do
   let(:chef_run) { ChefSpec::SoloRunner.new(step_into: ['consul_template_config']).converge(example_recipe) }
 
   describe 'create' do
+    it 'should create the consul_template_config' do
+      expect(chef_run).to create_consul_template_config('haproxy')
+    end
+
     it 'should create the config file' do
       expect(chef_run).to create_template('/etc/consul-template.d/haproxy')
     end
--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -13,3 +13,7 @@ end
 def install_golang_package(resource_name)
   ChefSpec::Matchers::ResourceMatcher.new(:golang_package, :install, resource_name)
 end
+
+def create_consul_template_config(resource_name)
+  ChefSpec::Matchers::ResourceMatcher.new(:consul_template_config, :create, resource_name)
+end
