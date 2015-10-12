dfea243054f5e1c4f0d2381bd39f379196e4f318
Matt Vinall <matt.vinall@imgtec.com>
2015-10-12 16:17:41 +0000

    support for consul-template v0.11.0

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -8,9 +8,18 @@
 #
 
 include_recipe 'ark::default'
+require 'chef/version_constraint'
 
 install_arch = node['kernel']['machine'] =~ /x86_64/ ? 'amd64' : '386'
-install_version = ['consul-template', node['consul_template']['version'], node['os'], install_arch].join('_')
+install_0_11 = Chef::VersionConstraint.new(">= 0.11.0").include?(node['consul_template']['version'])
+install_version = [
+  install_0_11 ? 'consul_template' : 'consul-template',
+  node['consul_template']['version'],
+  node['os'],
+  install_arch
+].join('_')
+install_compression = install_0_11 ? '.zip' : '.tar.gz'
+install_compression_path = install_0_11 ? '' : "#{install_version}/"
 install_checksum = node['consul_template']['checksums'].fetch(install_version)
 
 ark 'consul-template' do
@@ -19,8 +28,8 @@ ark 'consul-template' do
   checksum install_checksum
   url ::URI.join(node['consul_template']['base_url'],
                  "v#{node['consul_template']['version']}/",
-                 "#{install_version}.tar.gz").to_s
-  creates "#{install_version}/consul-template"
+                 "#{install_version}#{install_compression}").to_s
+  creates "#{install_compression_path}consul-template"
   action :cherry_pick
 end
 
