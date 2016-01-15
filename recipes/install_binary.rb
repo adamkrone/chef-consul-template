bc6ea4c021d944993645716b8211d8bcfec4077a
Adam Krone <krone.adam@gmail.com>
2016-01-15 11:48:15 -0600

    Reworks install_binary recipe
    - removes ark (uses libarchive instead)
    - fixes version updates using a symlink (Issue #38)
    - fixes issue that could cause a zero-length consul-template file to be
      created (Issue #40)
    - updates checksums to use releases.hashicorp.com for all version (also
      simplified install_binary logic)
    - adds new 0.12.2 version to checksums

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -7,33 +7,42 @@
 #
 #
 
-include_recipe 'ark::default'
+include_recipe 'libarchive::default'
 require 'chef/version_constraint'
 
 install_arch = node['kernel']['machine'] =~ /x86_64/ ? 'amd64' : '386'
-install_0_11 = Chef::VersionConstraint.new(">= 0.11.0").include?(node['consul_template']['version'])
 install_version = [
   'consul-template',
   node['consul_template']['version'],
   node['os'],
   install_arch
 ].join('_')
-install_compression = install_0_11 ? '.zip' : '.tar.gz'
-install_compression_path = install_0_11 ? '' : "#{install_version}/"
+install_extension = '.zip'
+install_file = install_version + install_extension
 install_checksum = node['consul_template']['checksums'].fetch(install_version)
-
-ark 'consul-template' do
-  path node['consul_template']['install_dir']
-  version node['consul_template']['version']
-  checksum install_checksum
-  url ::URI.join(node['consul_template']['base_url'],
+url = ::URI.join(node['consul_template']['base_url'],
                  "#{node['consul_template']['version']}/",
-                 "#{install_version}#{install_compression}").to_s
-  creates "#{install_compression_path}consul-template"
-  action :cherry_pick
+                 "#{install_version}#{install_extension}").to_s
+
+download_path = "#{Chef::Config['file_cache_path']}/#{install_file}"
+install_path = "#{node['consul_template']['install_dir']}/#{install_version}"
+
+remote_file download_path do
+  source url
+  checksum install_checksum
+  action :create_if_missing
 end
 
-file File.join(node['consul_template']['install_dir'], 'consul-template') do
-  mode '0755'
-  action :touch
+libarchive_file install_file do
+  path download_path
+  extract_to install_path
+  mode 0755
+  not_if { File.exist? install_path }
+  action :extract
+  notifies :restart, 'service[consul-template]', :delayed
+end
+
+link "#{node['consul_template']['install_dir']}/consul-template" do
+  to "#{install_path}/consul-template"
+  mode 0755
 end
