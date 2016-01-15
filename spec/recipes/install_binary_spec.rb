6f3f6fca8b36309e7d44ddda73817b1e305af2e5
Adam Krone <krone.adam@gmail.com>
2016-01-15 14:38:12 -0600

    More rubocop fixes

--- spec/recipes/install_binary_spec.rb
+++ spec/recipes/install_binary_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'consul-template::install_binary' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new(file_cache_path: '/var/chef/cache')
-      .converge('consul-template::default')
+                        .converge('consul-template::default')
   end
   let(:consul_template_zip) { "consul-template_#{chef_run.node['consul_template']['version']}_linux_amd64.zip" }
 
