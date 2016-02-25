1d87940a2982236cc0077cd8b565acc0aa1f85b5
Adam Krone <krone.adam@gmail.com>
2016-02-25 14:58:12 -0600

    Adds serverspec tests for install_only suite

---
+++ test/integration/install_only/serverspec/install_binary_spec.rb
@@ -0,0 +1,10 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe "Install from binary" do
+  describe file('/usr/local/bin/consul-template') do
+    it { should be_file }
+    it { should be_executable }
+  end
+end
