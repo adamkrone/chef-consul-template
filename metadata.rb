3a5b1b949223319534983dfcef5f64637f7bf330
Adam Krone <krone.adam@gmail.com>
2017-01-11 16:36:48 -0600

    v0.12.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'krone.adam@gmail.com'
 license          'Apache v2.0'
 description      'Installs/Configures consul-template'
 long_description 'Installs/Configures consul-template'
-version          '0.11.0'
+version          '0.12.0'
 
 recipe 'consul-template', 'Installs, configures, and starts the consul-template service.'
 recipe 'consul-template::install_binary', 'Installs consul-template from binary.'
