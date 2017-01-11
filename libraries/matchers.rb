2879fecd10e647a73b667e5996b429ad93328f44
Adam Krone <krone.adam@gmail.com>
2017-01-11 16:23:12 -0600

    Wrap entire matchers.rb in ChefSpec conditional

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,7 @@
-ChefSpec.define_matcher :consul_template_config if defined?(ChefSpec)
+if defined?(ChefSpec)
+  ChefSpec.define_matcher :consul_template_config if defined?(ChefSpec)
 
-def create_consul_template_config(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:consul_template_config, :create, resource_name)
+  def create_consul_template_config(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:consul_template_config, :create, resource_name)
+  end
 end
