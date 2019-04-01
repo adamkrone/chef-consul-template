38e97f1477aba13623bdbf85c1cac35632dd4454
Ashley Abbott <ashley.abbott@eagleeye.com>
2019-04-01 15:30:24 +0100

    added logic in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,11 @@ supports 'centos', '>= 6.9'
 supports 'arch'
 supports 'windows'
 
-depends 'libarchive', '>= 2.0.0'
+if Chef::VERSION.to_i <= 14
+  depends 'libarchive', '>= 2.0.0'
+else
+  depends 'libarchive', '<= 1.0.0'
+end
 depends 'golang'
 depends 'runit'
 depends 'nssm'
